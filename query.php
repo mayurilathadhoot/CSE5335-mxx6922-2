afe897e09eb2d250d88d1d7913ea84cddfe03f04
mayurilathadhoot <mayuri.latha@mavs.uta.edu>
2015-11-16 04:57:33 -0600

    Get Data
    Get data based on rating

--- query.php
+++ query.php
@@ -3,7 +3,7 @@
  * PHP PGSQL - How to insert rows into PostgreSQL table
  */
 // Connecting, selecting database
-$dbconn = pg_connect("host=ec2-107-21-223-147.compute-1.amazonaws.com dbname=d43brbvbt6ugl9 user=ywhsrpfxdaknkl password=dnYTje6Tvi_vDyGq9Z1Qsf5yhY")
+$dbconn = pg_connect("host=localhost dbname=webdata2 user=postgres password=webdata23")
         or die('Could not connect: ' . pg_last_error());
 		
 header('Content-Type: application/javascript;');
@@ -23,14 +23,23 @@ for ($i=1; $i<=9; $i++)
 	  $title = str_replace( array( '\'', '.' ), '', $title);
 	  //echo "imbdRating = " . $data['Episodes'][$j]['imdbRating']. "\n"; 
 	  $imbdrating = $data['Episodes'][$j]['imdbRating'];
+	}
+}
+	
 
 //perform the insert using pg_query
-$result = pg_query($dbconn, "INSERT INTO friends 
-                  VALUES( '$imbdid', '$title', $imbdrating  );");
+$result = pg_query($dbconn, "select title, imbdrating from friends where imbdrating = 8.5")
+or die("Error in query: $result." . pg_last_error($dbconn));
+	
+while ($row = pg_fetch_row($result)){
+  echo "title: $row[0] \n imbdrating: $row[1]\n";
+ // echo "<br />\n";
 //dump the result object
-//var_dump($result);
+//var_dump($row);
+
+
 }
-}
+
 // Closing connection
 pg_close($dbconn);
 ?>
