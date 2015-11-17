e02c9265dc25d58655bed40ff8c7803f1a8d17b3
mayurilathadhoot <mayuri.latha@mavs.uta.edu>
2015-11-17 15:28:37 -0600

    hello

--- new.php
+++ new.php
@@ -1,19 +1,3 @@
 <?php
-require "predis/autoload.php";
-Predis\Autoloader::register();
-
-try {
-	$redis = new PredisClient();
-
-	// This connection is for a remote server
-	
-		$redis = new PredisClient(array(
-		    "host" => "ec2-54-83-59-218.compute-1.amazonaws.com",
-		    "port" => 9969
-		));
-	
-}
-catch (Exception $e) {
-	die($e->getMessage());
-}
+$redis = new Predis\Client(getenv('redis://h:pfe2o6pqjl92sidbcgqbdtjaob5@ec2-54-83-59-218.compute-1.amazonaws.com:9969'));
 ?>
