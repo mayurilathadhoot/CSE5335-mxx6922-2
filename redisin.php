e2cb24161c518469eb357ba6e3b40da9b17b604f
mayurilathadhoot <mayuri.latha@mavs.uta.edu>
2015-11-17 09:27:33 -0600

    M
    M

---
+++ redisin.php
@@ -0,0 +1,9 @@
+<?php
+"require": {
+  ...
+  "predis/predis": "1.0.3",
+  ...
+}
+$redis = new Predis\Client(getenv('redis://h:pfe2o6pqjl92sidbcgqbdtjaob5@ec2-54-83-59-218.compute-1.amazonaws.com:9969'));
+
+?>
