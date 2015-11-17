f6cbdc284c02c5a53a7033685d01d596d19820b1
mayurilathadhoot <mayuri.latha@mavs.uta.edu>
2015-11-16 19:01:35 -0600

    extra credit
    extra credit

---
+++ extra.php
@@ -0,0 +1,25 @@
+
+<?php
+
+// Connecting, selecting database
+$dbconn = pg_connect("host=ec2-107-21-223-147.compute-1.amazonaws.com dbname=d43brbvbt6ugl9 user=ywhsrpfxdaknkl password=dnYTje6Tvi_vDyGq9Z1Qsf5yhY")
+        or die('Could not connect: ' . pg_last_error());
+		
+header('Content-Type: application/javascript;');
+
+parse_str(implode('&', array_slice($argv, 1)), $_GET);
+if (isset($_GET['imbdid']))
+{
+$result = pg_query($dbconn, "select imbdid, title, imbdrating from friends where imbdid = $_GET['imbdid']")	
+}
+else
+{
+$result = pg_query($dbconn, "select imbdid, title, imbdrating from friends")
+}
+	
+while ($row = pg_fetch_row($result)){
+  echo "imbdid: $row[0] \n title: $row[1]\n imbdrating: $row[2]\n";
+}
+// Closing connection
+pg_close($dbconn);
+?>
