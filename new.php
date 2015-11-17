7dfbdcae158eb3db3b6e482aa03650a0e58214ee
mayurilathadhoot <mayuri.latha@mavs.uta.edu>
2015-11-17 16:11:20 -0600

    hi

--- new.php
+++ new.php
@@ -1,5 +1,5 @@
 <?php
-$redis = new Predis\Client(getenv('redis://h:pfe2o6pqjl92sidbcgqbdtjaob5@ec2-54-83-59-218.compute-1.amazonaws.com:9969'));
+$redis = new Predis_Client(getenv('redis://h:pfe2o6pqjl92sidbcgqbdtjaob5@ec2-54-83-59-218.compute-1.amazonaws.com:9969'));
 
 
 ?>
