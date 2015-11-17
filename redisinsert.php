dbebaec30166d233d1be2cb595a136c688bd6115
mayurilathadhoot <mayuri.latha@mavs.uta.edu>
2015-11-17 13:25:31 -0600

    m

--- redisinsert.php
+++ redisinsert.php
@@ -1,7 +1,7 @@
 <?php
    //Connecting to Redis server on localhost
    $redis = new Redis();
-   $redis->connect('127.0.0.1', 6379);
+   $redis->connect('redis://h:pfe2o6pqjl92sidbcgqbdtjaob5@ec2-54-83-59-218.compute-1.amazonaws.com:9969', 9969);
    echo "Connection to server sucessfully";
    //check whether server is running or not
    echo "Server is running: "+ $redis->ping();
