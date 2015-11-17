f53da3788cc1ad87fbb04c693cfab4e1c3e60219
mayurilathadhoot <mayuri.latha@mavs.uta.edu>
2015-11-16 23:18:31 -0600

    final

--- extra.php
+++ extra.php
@@ -13,15 +13,14 @@ $line = fgets($handle);
 $line = str_replace("\n", "", $line);
 $line = str_replace("\r", "", $line);
 $x="'".$line."'";
-if (!empty($result)){
+if (!empty($x)){
 	$result = pg_query($dbconn, "SELECT title, imbdrating FROM friends WHERE imbdid = $x ");
-	
+	if (!empty($result)){
 	while ($row = pg_fetch_row($result)){
-	if (!empty($row)){	
     echo "Title: $row[0] --- imbdrating: $row[1]\n";}
+	}
 	else{
 		echo "please enter a correct imbdid";
-	}
 	}
 }
 else{
