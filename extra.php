ece29691b930639d42d9c9ce861c443e71f573b8
mayurilathadhoot <mayuri.latha@mavs.uta.edu>
2015-11-16 20:02:29 -0600

    hello
    hello

--- extra.php
+++ extra.php
@@ -11,6 +11,7 @@ header('Content-Type: application/javascript;');
 echo "Please enter imbdid : ";
 $handle = fopen ("php://stdin","r");
 $line = fgets($handle);
+echo $line;
 if(isset($line))
 {
 $result = pg_query($dbconn, "select title, imbdrating from friends where imbdid = {$line} ");	
